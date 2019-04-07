a86ef4fb5f64678f6d6581da77e8bceb7be836f7
ShaBang <baptiste.perrier@protonmail.com>
2019-04-08 00:04:39 +0200

    Added an error dialog box. Errors handling : unreachable server, non-renewed data

--- TPInterface/client/web_scripts/main.js
+++ TPInterface/client/web_scripts/main.js
@@ -1,4 +1,15 @@
+function print_error(title, message){
+  $("#error").css("display", "block");
+  $("#err_title").text(title);
+  $("#err_content").text(message);
+}
+
+function hide_error(){
+  $("#error").css("display", "none");
+}
+
 $(function(){
+    var oldID = 0;
     var tid = setInterval(function(){
         $.ajax({
             url: 'http://localhost/EducEco/TPInterface/server/get_data.php',
@@ -9,10 +20,16 @@ $(function(){
             $("#title").text(data.type + " :");
             $("#value").text(data.value);
             $("#unit").text(data.unit);
-            console.log(data);
+
+            if(data.id == oldID){
+              print_error("Données non-renouvellées", "Les données renvoyées par le server ne sont pas renouvellées, vérifiez que le parser est démarré, que le montage est relié à l'ordinateur et la base de données accessible.");
+            } else {
+              hide_error();
+            }
+            oldID = data.id;
           })
           .fail(function(err) {
-            console.log('Error: ' + err.status);
+            print_error("Données inateignables", "Le serveur ne renvoie pas de données, vérifiez la disponobilité du server PHP.")
           });
     }, 500);
 
