dcba643e5ee04c3a87b0a441a7e51d8d279ea734
FeeeZ <baptiste.perrier@protonmail.com>
2019-10-02 15:17:58 +0200

    db test

--- dbTest.py
+++ dbTest.py
@@ -1,8 +1,17 @@
-import mcp3008
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 import time 
+#MCP INIT
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs = digitalio.DigitalInOut(board.D5)
+mcp = MCP.MCP3008(spi, cs)
 
-with mcp3008.MCP3008() as adc:
-#adc.read(mcp3008.CH0)
+ch0 = AnalogIn(mcp, MCP.P0)
+
+#DB INIT
 msTimestamp = lambda: int(round(time.time() * 1000))
 cnx = mysql.connector.connect(user='root', database='EDUCECO')
 cursor = cnx.cursor()
@@ -10,8 +19,8 @@ cursor = cnx.cursor()
 add_intensite = ("INSERT INTO Intensity "
                 "(Value, TIMESTAMP)"
                 "VALUES (%s, %s)")
-while true:
-    cursor.execute(add_intensite, (adc.read(mcp3008.CH0), msTimestamp))
+while True:
+    cursor.execute(add_intensite, (ch0.value, msTimestamp))
     time.sleep(1)
     cnx.commit()
 
